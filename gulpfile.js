c2670f17b87177831fa23ca645ae1fc8d6db88a3
Dan O. Williams <daniel.williams@gsa.gov>
2018-10-03 13:57:43 -0700

    Add initial gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,135 @@
+/*
+* * * * * ==============================
+* * * * * ==============================
+* * * * * ==============================
+* * * * * ==============================
+========================================
+========================================
+========================================
+----------------------------------------
+USWDS SASS GULPFILE
+----------------------------------------
+*/
+
+var autoprefixer  = require('autoprefixer');
+var cssnano       = require('cssnano');
+var del           = require('del');
+var gulp          = require('gulp');
+var mqpacker      = require('css-mqpacker');
+var notify        = require('gulp-notify');
+var path          = require('path');
+var pkg           = require('./node_modules/uswds/package.json');
+var postcss       = require('gulp-postcss');
+var replace       = require('gulp-replace');
+var sass          = require('gulp-sass');
+var sourcemaps    = require('gulp-sourcemaps');
+
+/*
+----------------------------------------
+LOCATIONS
+----------------------------------------
+- All locations are relative to the
+  project root
+- Don't use a trailing `/` for path
+  names
+----------------------------------------
+*/
+
+// USWDS source directory
+const USWDS_SRC = 'node_modules/uswds/dist';
+
+// Project Sass source directory
+const PROJECT_SASS_SRC = './path/to/project/sass';
+
+// Images destination
+const IMG_DEST = './path/to/images/destination';
+
+// Fonts destination
+const FONTS_DEST = './path/to/fonts/destination';
+
+// Javascript destination
+const JS_DEST = './path/to/js/destination';
+
+// Compiled CSS destination
+const CSS_DEST = './path/to/css/destination';
+
+/*
+----------------------------------------
+SETTINGS
+----------------------------------------
+*/
+
+const AUTOPREFIXER_OPTIONS = [
+  '> 1%',
+  'Last 2 versions',
+  'IE 11',
+];
+
+/*
+----------------------------------------
+TASKS
+----------------------------------------
+*/
+
+gulp.task('clean-css', function () {
+  return del([
+    `${CSS_DEST}/**/*`
+  ]);
+});
+
+gulp.task('copy-uswds-fonts', () => {
+  return gulp.src(`${USWDS_SRC}/fonts/**/**`)
+  .pipe(gulp.dest(`${FONTS_DEST}`));
+});
+
+gulp.task('copy-uswds-images', () => {
+  return gulp.src(`${USWDS_SRC}/js/**/**`)
+  .pipe(gulp.dest(`${IMG_DEST}`));
+});
+
+gulp.task('copy-uswds-js', () => {
+  return gulp.src(`${USWDS_SRC}/img/**/**`)
+  .pipe(gulp.dest(`${JS_DEST}`));
+});
+
+gulp.task('build-sass', function (done) {
+  var plugins = [
+    autoprefixer(AUTOPREFIXER_OPTIONS),
+    mqpacker({ sort: true }),
+    cssnano(({ autoprefixer: { browsers: AUTOPREFIXER_OPTIONS }}))
+  ];
+  return gulp.src([
+      `${PROJECT_SASS_SRC}/*.scss`
+    ])
+    .pipe(replace(
+      /\buswds @version\b/g,
+      'uswds v' + pkg.version
+    ))
+    .pipe(sourcemaps.init({ largeFile: true }))
+    .pipe(sass({
+        includePaths: [
+          `${PROJECT_SASS_SRC}`,
+          `${USWDS_SRC}/scss`,
+          `${USWDS_SRC}/scss/packages`,
+        ]
+      }))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest(`${CSS_DEST}`))
+    .pipe(sourcemaps.write('.'))
+    .pipe(notify({
+      "sound": "Pop" // case sensitive
+    }));
+});
+
+gulp.task('init', gulp.series(
+  'copy-uswds-fonts',
+  'copy-uswds-images',
+  'copy-uswds-js',
+  'build-sass',
+));
+
+gulp.task('watch', gulp.series('build-sass', function (){
+  gulp.watch(`${PROJECT_SASS_SRC}/**/*.scss`, gulp.series('build-sass'));
+}));
+
+gulp.task('default', gulp.series('watch', 'build-sass'));
